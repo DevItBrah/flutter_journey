965a4cdca973c88d5117622987662708c6d039c5
Justaguy <nextaiguy@gmail.com>
2025-04-26 10:56:34 +0530

    third commit

--- lib/utils/theme/custom_theme/checkbox_theme.dart
+++ lib/utils/theme/custom_theme/checkbox_theme.dart
@@ -8,10 +8,35 @@ class TCheckboxTheme{
     shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(4)),
     checkColor: MaterialStateProperty.resolveWith((states){
       if (states.contains(MaterialState.selected)){
-        return(Colors.white);
+        return Colors.white;
       }else{
-        return Colors.transparent;
+        return Colors.black;
       }
-    })
-  );
+    }),
+    fillColor:MaterialStateProperty.resolveWith((states){
+  if (states.contains(MaterialState.selected)){
+  return Colors.blue;
+  }else{
+  return Colors.transparent;
+  }
+}));
+
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData (
+      shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(4)),
+      checkColor: MaterialStateProperty.resolveWith((states){
+        if (states.contains(MaterialState.selected)){
+          return Colors.white;
+        }else{
+          return Colors.black;
+        }
+      }),
+      fillColor:MaterialStateProperty.resolveWith((states){
+        if (states.contains(MaterialState.selected)){
+          return Colors.blue;
+        }else{
+          return Colors.transparent;
+        }
+      }));
+
+
 }
