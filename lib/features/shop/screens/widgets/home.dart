dcd7f6f9627e25bb91d9933d9b7265a288798b2c
PikaPush <thisisamaanab@gmail.com>
2025-04-29 20:17:21 -0700

    custome home screen shape defined

--- lib/features/shop/screens/widgets/home.dart
+++ lib/features/shop/screens/widgets/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/common/widgets/custom_shapes/containers/curverd_edges/curved_edges.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 
@@ -12,27 +13,30 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              height: 400,
-              color:TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 100,
-                    right: 100,
-                    child: TCircularContainer(
-                      backgroundColor: TColors.textWhite.withValues(alpha: 0.1),
+            ClipPath(
+              clipper: TCustomCurvedEdges(),
+              child: Container(
+                height: 400,
+                color:TColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 100,
+                      right: 100,
+                      child: TCircularContainer(
+                        backgroundColor: TColors.textWhite.withValues(alpha: 0.1),
+                      ),
                     ),
-                  ),
-                  Positioned(
-                    top: 50,
-                    right: 50,
-                    child: TCircularContainer(
-                      backgroundColor: TColors.textWhite.withValues(alpha: 0.1),
+                    Positioned(
+                      top: 50,
+                      right: 50,
+                      child: TCircularContainer(
+                        backgroundColor: TColors.textWhite.withValues(alpha: 0.1),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             )
           ],
--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -2,9 +2,6 @@ import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:iconsax/iconsax.dart';
 
 import 'features/shop/screens/widgets/home.dart';
