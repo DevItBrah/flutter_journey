18ac6534f8806ff7ef7e30e2b15e046f5fd591c0
PikaPush <thisisamaanab@gmail.com>
2025-05-15 15:25:09 +0530

    42 without exception

--- lib/utils/exceptions/firebase_auth_exceptions.dart
+++ lib/utils/exceptions/firebase_auth_exceptions.dart
@@ -5,9 +5,9 @@ class TFirebaseAuthException implements Exception{
   String get message{
     switch(code){
       case 'email-already-in-use':
-        return'The email address is already registerrd.please use a different email.';
+        return'The email address is already registered.please use a different email.';
       case 'invalid-email':
-        return'Them email address is provided is invalid. Plsease enter a valid email';
+        return'Them email address is provided is invalid. Please enter a valid email';
       case 'weak-password':
         return'The password is too weak. please choose a stronger password.';
       case 'user-disabled':
@@ -17,16 +17,16 @@ class TFirebaseAuthException implements Exception{
       case 'Wrong password':
         return'Incorrect password. please check your password and try again.';
       case 'Invalid-verification-code':
-        return'Invalid verificaton ID. Please request a new verification code.';
+        return'Invalid verification ID. Please request a new verification code.';
       case 'Quota exceeded':
         return'Quota exceeded.Please try again later.';
       case 'email-already-exists':
-        return'The email address already existes. Please use different email address.';
+        return'The email address already exists. Please use different email address.';
       case'Provider-already-linked':
         return'The account is already linked with another provider.';
       case'requires-recent-login':
         return 'This operation is sensitive adn requires recent authentication. Please login again.';
-      case'credential-alrady-in-use':
+      case'credential-already-in-use':
         return 'This credential is already associated with a different user account.';
       case'user-mismatch':
         return'The supplied credentials do not correspond to the previously signed in user.';
@@ -38,7 +38,7 @@ class TFirebaseAuthException implements Exception{
         return 'The action code has expired. please request a new action code';
       case 'invalid-action-code':
         return'The action code is invalid. Please check the code and try again';
-      case 'mmissing-action-code':
+      case 'missing-action-code':
         return 'The action code is missing. Please provide a valid action code.';
       case 'user-token-expired':
         return 'a';
